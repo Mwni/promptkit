19f09c0ebdf2504956c7e8764cf9765aaef6004a
Mwni <manuel@tigmar.de>
2023-05-22 06:06:49 +0200

    make load_yaml respect caller's dir

--- langstack/prompts.py
+++ langstack/prompts.py
@@ -1,17 +1,23 @@
+import os
 import yaml
+import inspect
 from langchain.schema import HumanMessage
 
 
 class Struct:
-    def __init__(self, **entries):
-        for k, v in entries.items():
-            if isinstance(k, (list, tuple)):
-                setattr(self, k, [Struct(**x) if isinstance(x, dict) else x for x in v])
-            else:
-                setattr(self, k, Struct(**v) if isinstance(v, dict) else v)
+	def __init__(self, **entries):
+		for k, v in entries.items():
+			if isinstance(k, (list, tuple)):
+				setattr(self, k, [Struct(**x) if isinstance(x, dict) else x for x in v])
+			else:
+				setattr(self, k, Struct(**v) if isinstance(v, dict) else v)
 
 
 def load_yaml(path):
+	caller_file = inspect.stack()[1].filename
+	caller_dir = os.path.dirname(caller_file)
+	path = os.path.join(caller_dir, path)
+
 	with open(path) as f:
 		return Struct(**yaml.safe_load(f))
 
