5e72dec54340a3e67ab1a38458e6158f0e97a57d
Mwni <manuel@tigmar.de>
2023-08-01 06:23:29 +0200

    implement journal mismatch popping option

--- langstack/journal.py
+++ langstack/journal.py
@@ -1,12 +1,16 @@
 from .messages import message_to_dict, dict_to_message
+from .log import make_logger
+
+log = make_logger('langstack')
 
 
 class Journal:
 	@classmethod
-	def from_dict(cls, dict):
+	def from_dict(cls, dict, pop_mismatch=False):
 		journal = Journal(function=dict['function'], file=dict['file'])
 		journal.cache = dict['cache']
 		journal.replay_index = 0
+		journal.pop_mismatch = pop_mismatch
 		return journal
 
 
@@ -15,21 +19,23 @@ class Journal:
 		self.file = file
 		self.cache = []
 		self.replay_index = None
+		self.pop_mismatch = False
 
 
 	def llm_result(self, key, stack, input):
 		stack = self.trim_stack(stack)
 		stack = [{'function': call.function, 'file': call.filename} for call in stack]
 		input = [message_to_dict(m) for m in input]
+		add_to_cache = lambda output: self.cache.append({
+			'stack': stack,
+			'type': 'llm',
+			'key': key,
+			'input': input,
+			'output': message_to_dict(output)
+		})
 
-		if self.replay_index is None:
-			return None, lambda output: self.cache.append({
-				'stack': stack,
-				'type': 'llm',
-				'key': key,
-				'input': input,
-				'output': message_to_dict(output)
-			})
+		if self.replay_index is None or len(self.cache) == 0:
+			return None, add_to_cache
 		else:
 			try:
 				cache = self.cache[self.replay_index]
@@ -40,7 +46,18 @@ class Journal:
 				assert all(a == b for a, b in zip(input, cache['input'])), 'different input'
 				
 			except Exception as e:
-				raise Exception('replay cache mismatch: %s' % str(e))
+				message = 'replay cache mismatch: %s' % str(e)
+
+				if self.pop_mismatch:
+					log.warn(message)
+					log.info('popping history and continuing fresh from here on')
+
+					self.cache = self.cache[0:self.replay_index]
+					self.replay_index = None
+
+					return None, add_to_cache
+				else:
+					raise Exception(message)
 			
 			self.replay_index += 1
 
