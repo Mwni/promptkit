0eff19a9b46a279288b3c5c5f60ff7e252ffed56
Mwni <manuel@tigmar.de>
2024-01-15 11:21:10 +0100

    rename journal "cache" to "graph"

--- promptkit/journal.py
+++ promptkit/journal.py
@@ -8,7 +8,7 @@ class Journal:
 	@classmethod
 	def from_dict(cls, dict, pop_mismatch=False):
 		journal = Journal(function=dict['function'], file=dict['file'])
-		journal.cache = dict['cache']
+		journal.graph = dict['graph']
 		journal.replay_index = 0
 		journal.pop_mismatch = pop_mismatch
 		return journal
@@ -17,7 +17,7 @@ class Journal:
 	def __init__(self, function, file):
 		self.function = function
 		self.file = file
-		self.cache = []
+		self.graph = []
 		self.replay_index = None
 		self.pop_mismatch = False
 
@@ -26,7 +26,7 @@ class Journal:
 		stack = self.trim_stack(stack)
 		stack = [{'function': call.function, 'file': call.filename} for call in stack]
 		input = [message_to_dict(m) for m in input]
-		add_to_cache = lambda output: self.cache.append({
+		add_to_graph = lambda output: self.graph.append({
 			'stack': stack,
 			'type': 'llm',
 			'key': key,
@@ -34,37 +34,37 @@ class Journal:
 			'output': message_to_dict(output)
 		})
 
-		if self.replay_index is None or len(self.cache) == 0:
-			return None, add_to_cache
+		if self.replay_index is None or len(self.graph) == 0:
+			return None, add_to_graph
 		else:
 			try:
-				cache = self.cache[self.replay_index]
+				graph = self.graph[self.replay_index]
 
-				assert cache['type'] == 'llm', 'wrong type'
-				assert cache['key'] == key, 'wrong key'
-				assert all(a == b for a, b in zip(stack, cache['stack'])), 'different stack'
-				assert all(a == b for a, b in zip(input, cache['input'])), 'different input'
+				assert graph['type'] == 'llm', 'wrong type'
+				assert graph['key'] == key, 'wrong key'
+				assert all(a == b for a, b in zip(stack, graph['stack'])), 'different stack'
+				assert all(a == b for a, b in zip(input, graph['input'])), 'different input'
 				
 			except Exception as e:
-				message = 'replay cache mismatch: %s' % str(e)
+				message = 'replay graph mismatch: %s' % str(e)
 
 				if self.pop_mismatch:
 					log.warn(message)
 					log.info('popping history and continuing fresh from here on')
 
-					self.cache = self.cache[0:self.replay_index]
+					self.graph = self.graph[0:self.replay_index]
 					self.replay_index = None
 
-					return None, add_to_cache
+					return None, add_to_graph
 				else:
 					raise Exception(message)
 			
 			self.replay_index += 1
 
-			if self.replay_index >= len(self.cache):
+			if self.replay_index >= len(self.graph):
 				self.replay_index = None
 			
-			return dict_to_message(cache['output']), None
+			return dict_to_message(graph['output']), None
 
 
 	def trim_stack(self, stack):
@@ -79,6 +79,6 @@ class Journal:
 		return {
 			'function': self.function,
 			'file': self.file,
-			'cache': self.cache
+			'graph': self.graph
 		}
 	
